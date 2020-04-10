826581e9b0a0490b81e7bc9cc0c91ffc11d02000
Abdulkader Freij <abdulkader@arch.localdomain>
2020-04-10 09:30:40 +0300

    add routes

--- routes/api.php
+++ routes/api.php
@@ -30,6 +30,19 @@ Route::name('api.')->namespace('Api')->group(function () {
         Route::namespace('Auth')->group(function () {
             Route::get('me', 'MeController@me')->name('me');
             Route::post('logout', 'LogoutController@logout')->name('logout');
+            Route::get('/currencies', 'CurrencyController@index')->name('currencies.all');
+
+Route::get('/transactions', 'TransactionController@index')->name('transactions.all');
+Route::post('/transactions', 'TransactionController@store')->name('transactions.store');
+Route::get('/transactions/{transaction}', 'TransactionController@show')->name('transactions.show');
+Route::put('/transactions/{transaction}', 'TransactionController@update')->name('transactions.update');
+Route::delete('/transactions/{transaction}', 'TransactionController@destroy')->name('transactions.destroy');
+
+Route::get('/categories', 'CategoryController@index')->name('categories.all');
+Route::post('/categories', 'CategoryController@store')->name('categories.store');
+Route::get('/categories/{category}', 'CategoryController@show')->name('categories.show');
+Route::put('/categories/{category}', 'CategoryController@update')->name('categories.update');
+Route::delete('/categories/{category}', 'CategoryController@destroy')->name('categories.destroy');
         });
     });
 });
